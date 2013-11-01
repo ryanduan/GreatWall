69d017c63a8d14cfdf219635282d43d2a478ec9f
ryanduan <ryan@deliveryherochina.com>
2013-11-01 20:21:27 +0800

    add base template

--- easyauto/tech/views.py
+++ easyauto/tech/views.py
@@ -23,4 +23,10 @@ def tech(req):
 def julia(req):
     tem = 'julia_learning.html'
     cont = {}
+    julia = TechKind.objects.get(kind_name="Julia")
+    blogs = TechBlog.objects.filter(kind=julia)
+    cont.update({
+                "julia": julia,
+                "blogs": blogs,
+                })
     return render_to_response(tem, cont)
