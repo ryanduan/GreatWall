d23a1b8bf1b9a85b1e2be2b20499be823774ce4d
awang <dyw564@gmail.com>
2013-12-09 16:17:47 +0800

    tornado first project

---
+++ homedecor/webmanage.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello world")
+
+class StoryHandler(tornado.web.RequestHandler):
+    def get(self, story_id):
+        self.write("You requested the story" + story_id)
+
+class MyFormHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write('<html><body><form action="/myform" method="post">'
+                   '<input type="text" name="message">'
+                   '<input type="submit" value="Submit">'
+                   '</form></body></html>')
+
+    def post(self):
+        self.set_header("Content-Type", "text/plain")
+        self.write("You wrote " + self.get_argument("message"))
+
+
+application = tornado.web.Application([
+    (r"/", MainHandler),
+    (r"/story/([0-9]+)", StoryHandler),
+    (r"/myform/", MyFormHandler),
+])
+
+if __name__ == "__main__":
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
